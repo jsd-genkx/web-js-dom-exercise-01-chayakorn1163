ca8d865f7c4562ec8790b10aad9c94489ed54272
chayakon11643 <chayakon.prathumchai@gmail.com>
2024-05-20 14:12:16 +0700

    First Commit

---
+++ Script.js
@@ -0,0 +1,55 @@
+/*function ChangeText() {
+let elementH2 = document.getElementById("mydoing");
+    
+    if (elementH2) {
+        elementH2.innerHTML = " I am working with JavaScript and I am working with";
+    }
+    else {
+        elementH2.innerHTML = " god bye!";
+    }
+
+    
+}*/
+
+/*let toggle = true; //ประกาศตัวแปร และกำหนดเป้น true
+
+function ChangeText() {
+    let elementH2 = document.getElementById("mydoing");
+    if (toggle) {
+        elementH2.innerHTML = "I am learning JavaScript and I am working with JavaScript";
+        elementH2.innerHTML = "hi my name is JavaScript"
+
+    } else {
+        elementH2.innerHTML = "Good bye";
+    }
+
+    toggle = !sumtext; // เปลี่ยนสถานะ
+}*/
+
+let counter = 0; // ตัวแปรนับสำหรับติดตามลำดับข้อความ
+
+/*function ChangeText() {
+    let elementH2 = document.getElementById("mydoing"); // ค้นหา element ที่มี id="mydoing"
+    switch (counter) {
+        case 0:
+            elementH2.innerHTML = "I am learning JavaScript";
+            break;
+        case 1:
+            elementH2.innerHTML = "I am working with JavaScript";
+            break;
+        case 2:
+            elementH2.innerHTML = "JavaScript is fun";
+            break;
+        case 3:
+            elementH2.innerHTML = "Good bye";
+            break;
+    }
+    counter = (counter + 1) % 4; // เพิ่มค่า counter และวนกลับไปที่ 0 เมื่อถึง 4
+}*/
+
+
+function ChangeText (){
+    let elementH1 = document.getElementById("mydoing");
+    elementH1.innerHTML = "I am learning JavaScript";
+
+}
